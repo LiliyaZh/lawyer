af75eef80272906598a2e357f9ff594efe74679e
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 14:13:35 +0300

    Добавлен скрипт экспорта таблицы фильтрации заявок

--- php/requests.php
+++ php/requests.php
@@ -97,3 +97,28 @@
         <?php endforeach; ?>
     </tbody>
 </table>
+<script>
+    function exportTableToCSV(filename) {
+        var csv = [];
+        var rows = document.querySelectorAll("table#requests tr");
+
+        for (var i = 0; i < rows.length; i++) {
+            var row = [], cols = rows[i].querySelectorAll("td, th");
+
+            for (var j = 0; j < cols.length - 1; j++)
+                row.push(cols[j].innerText);
+
+            csv.push(row.join(","));
+        }
+
+        // Сохранение в файл
+        var csvContent = "data:text/csv;charset=utf-8," + csv.join("\n");
+        var encodedUri = encodeURI(csvContent);
+        var link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", filename);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link); // Удаление элемента после скачивания
+    }
+</script>
