764e356b1f24c76f13047b6c4d7082a28a7b7ce9
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 20:13:09 +0300

    Добавлена загрузка данных на страницу сотрудников

--- php/workers.php
+++ php/workers.php
@@ -1,3 +1,17 @@
+<?php
+require_once ('db.php');
+
+if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] != 'employee' || $_SESSION['empl_type'] != 1) {
+    header('Location: home');
+    exit;
+}
+
+// Получение списка сотрудников
+$sql = "SELECT * FROM workers_in_company";
+$stmt = $pdo->query($sql);
+$workers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <h2 class="mb-4 text-center">Сотрудники ОГКУ «Государственное юридическое бюро»</h2>
 <a href="addworker" class="btn btn-primary mb-3">Добавить сотрудника</a>
 <table class="table table-striped">
