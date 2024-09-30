c2b55e6c7fd8959e5509127af8d046dbf8716078
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 12:00:56 +0300

    Составлена страница тестовых аккаунтов

---
+++ php/accounts.php
@@ -0,0 +1,16 @@
+<h2 class="mb-4 text-center">Тестовые аккаунты в системе</h2>
+<style>
+td1{border:1px solid #000; padding:10px;}
+</style>
+<table class="table table-striped table-hover">
+<thead>
+<tr><th scope="col">Роль</td><th scope="col">Email (логин)</td><th scope="col">Пароль</td></tr>
+</thead>
+<tbody>
+<tr><td>Клиент</td><td>viktor.ivanov@yandex.ru</td><td>123456</td></tr>
+<tr><td>Администратор (начальник юридического отдела)</td><td>andrew.gundarov@yandex.ru</td><td>123456</td></tr>
+<tr><td>Юристконсульт</td><td> mikle.velesov@yandex.ru</td><td>123456</td></tr>
+<tr><td>Юристконсульт</td><td>val.obrushkin@yandex.ru</td><td>123456</td></tr>
+<tr><td>Делопроизводитель</td><td>serg.lipoedov@yandex.ru</td><td>123456</td></tr>
+</tbody>
+</table>
