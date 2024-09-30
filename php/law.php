9ead5dc8d0851fe79d4c3d14e0fbccde1c50b2c9
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 11:23:06 +0300

    Создана страница законодательных актов

---
+++ php/law.php
@@ -0,0 +1,53 @@
+<h2 class="mb-4 text-center">Основные законодательные акты Российской Федерации</h2>
+<ul class="list-group">
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_28399/" target="_blank" class="text-decoration-none">
+            Конституция Российской Федерации
+        </a> - Основной закон Российской Федерации, определяющий правовую и политическую систему страны.
+    </li>
+    <li class="list-group-item">
+        <a href="" target="_blank" class="text-decoration-none">
+            Гражданский кодекс Российской Федерации (ГК РФ)
+        </a> - Основной документ, регулирующий гражданско-правовые отношения в РФ.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_10699/" target="_blank" class="text-decoration-none">
+            Уголовный кодекс Российской Федерации (УК РФ)
+        </a> - Закон, устанавливающий уголовную ответственность за преступления на территории РФ.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_10699/" target="_blank" class="text-decoration-none">
+            Налоговый кодекс Российской Федерации (НК РФ)
+        </a> - Законодательный акт, регулирующий налоговые отношения в стране.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_10671/" target="_blank" class="text-decoration-none">
+            Трудовой кодекс Российской Федерации (ТК РФ)
+        </a> - Основной закон, регулирующий трудовые отношения между работниками и работодателями.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_34481/" target="_blank" class="text-decoration-none">
+            Кодекс Российской Федерации об административных правонарушениях (КоАП РФ)
+        </a> - Закон, устанавливающий ответственность за административные правонарушения.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_5142/" target="_blank" class="text-decoration-none">
+            Семейный кодекс Российской Федерации (СК РФ)
+        </a> - Закон, регулирующий семейные отношения: брак, развод, права и обязанности супругов.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_10672/" target="_blank" class="text-decoration-none">
+            Жилищный кодекс Российской Федерации (ЖК РФ)
+        </a> - Закон, регулирующий жилищные отношения, связанные с владением и пользованием жильем.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_5144/" target="_blank" class="text-decoration-none">
+            Закон "О полиции"
+        </a> - Закон, определяющий функции, полномочия и обязанности полиции на территории РФ.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.consultant.ru/document/cons_doc_LAW_16310/" target="_blank" class="text-decoration-none">
+            Закон "О защите прав потребителей"
+        </a> - Законодательный акт, защищающий права потребителей в сфере торговли и услуг.
+    </li>
+</ul>
