566fd30a133f82a34bb349e1635e89dd4e3990b8
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 12:14:06 +0300

    Добавлена форма обратной связи

---
+++ php/newfeedback.php
@@ -0,0 +1,7 @@
+<form action="" method="POST">
+    <div class="mb-3">
+        <label for="message" class="form-label">Оставьте Ваше сообщение</label>
+        <textarea class="form-control" id="message" name="message" rows="10" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Отправить</button>
+</form>
