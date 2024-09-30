75548d9f310d323616700017851b126da097c97f
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 14:17:57 +0300

    Добавление каркаса функции фильтрации

--- php/requests.php
+++ php/requests.php
@@ -1,3 +1,46 @@
+<?php
+require_once ('db.php');
+
+function fetchRequests($pdo, $filters)
+{
+    $query = "SELECT 
+                cr.date_time_of_request, 
+                rt.name_of_type, 
+                rs.name_of_status, 
+                c.name_of_client, 
+                c.surname_of_client, 
+                c.telephone_of_client, 
+                e.name_of_worker, 
+                e.surname_of_worker, 
+                e.telephone_of_worker,
+                cr.key_of_request
+            FROM requests_of_clients cr
+            LEFT JOIN types_of_requests rt ON cr.key_of_type = rt.key_of_type
+            JOIN statuses_of_requests rs ON cr.key_of_status = rs.key_of_status
+            JOIN clients_of_company c ON cr.key_of_client = c.key_of_client
+            LEFT JOIN workers_in_company e ON cr.key_of_worker = e.key_of_worker
+            WHERE 1=1";
+
+    $params = [];
+    if (isset($_SESSION['empl_type']) && $_SESSION['empl_type'] == 3) { // Юристконсульт
+        $query .= " AND cr.key_of_worker = :key_of_worker";
+        $params[':key_of_worker'] = $_SESSION['user_id'];
+    }
+
+$filters = [
+    'key_of_status' => $_GET['key_of_status'] ?? '',
+    'key_of_type' => $_GET['key_of_type'] ?? '',
+    'key_of_client' => $_GET['key_of_client'] ?? '',
+    'key_of_worker' => $_GET['key_of_worker'] ?? '',
+    'date_from' => $_GET['date_from'] ?? '',
+    'date_to' => $_GET['date_to'] ?? ''
+];
+
+$requests = fetchRequests($pdo, $filters);
+
+?>
+
+
 <h1 class="text-center">Заявки на оказание бесплатной юридической помощи</h1>
 <form method="POST" class="mb-4">
     <div class="row mb-3">
