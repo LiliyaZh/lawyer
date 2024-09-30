ff8f0965b23e5115fdd3322bc318e8c4b5197f54
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 11:25:19 +0300

    Создана страница ссылок на законодательные акты

---
+++ php/government.php
@@ -0,0 +1,54 @@
+<h2 class="mb-4 text-center">Основные государственные порталы Российской Федерации</h2>
+<ul class="list-group">
+    <li class="list-group-item">
+        <a href="https://www.gosuslugi.ru" target="_blank" class="text-decoration-none">
+            Единый портал государственных услуг (Госуслуги)
+        </a> - Портал для получения государственных и муниципальных услуг в электронном виде.
+    </li>
+    <li class="list-group-item">
+        <a href="https://www.nalog.ru" target="_blank" class="text-decoration-none">
+            Федеральная налоговая служба (ФНС России)
+        </a> - Сайт для взаимодействия с налоговыми органами, уплаты налогов, подачи деклараций.
+    </li>
+    <li class="list-group-item">
+        <a href="https://www.pfrf.ru" target="_blank" class="text-decoration-none">
+            Пенсионный фонд России (ПФР)
+        </a> - Официальный сайт ПФР для получения информации о пенсионном обеспечении, социальных выплатах и др.
+    </li>
+    <li class="list-group-item">
+        <a href="http://www.government.ru" target="_blank" class="text-decoration-none">
+            Правительство Российской Федерации
+        </a> - Официальный сайт Правительства РФ, с новостями, документами и информацией о деятельности правительства.
+    </li>
+    <li class="list-group-item">
+        <a href="https://minzdrav.gov.ru/" target="_blank" class="text-decoration-none">
+            Министерство здравоохранения Российской Федерации
+        </a> - Портал Минздрава РФ с информацией о здравоохранении, медицинских услугах и законодательстве.
+    </li>
+    <li class="list-group-item">
+        <a href="https://www.mvd.ru" target="_blank" class="text-decoration-none">
+            Министерство внутренних дел Российской Федерации (МВД)
+        </a> - Официальный сайт МВД РФ с информацией о полиции, безопасности и правоохранительных органах.
+    </li>
+    <li class="list-group-item">
+        <a href="https://www.rzd.ru" target="_blank" class="text-decoration-none">
+            Российские железные дороги (РЖД)
+        </a> - Сайт РЖД для покупки билетов, информации о расписании поездов и состоянии железнодорожного транспорта.
+    </li>
+    <li class="list-group-item">
+        <a href="https://zakupki.gov.ru" target="_blank" class="text-decoration-none">
+            Единая информационная система в сфере закупок (Закупки)
+        </a> - Портал для государственных и муниципальных закупок.
+    </li>
+    <li class="list-group-item">
+        <a href="https://rosstat.gov.ru" target="_blank" class="text-decoration-none">
+            Федеральная служба государственной статистики (Росстат)
+        </a> - Официальный сайт Росстата для доступа к данным государственной статистики.
+    </li>
+    <li class="list-group-item">
+        <a href="https://rkn.gov.ru" target="_blank" class="text-decoration-none">
+            Федеральная служба по надзору в сфере связи, информационных технологий и массовых коммуникаций
+            (Роскомнадзор)
+        </a> - Портал Роскомнадзора с информацией о контроле и надзоре в сфере связи и СМИ.
+    </li>
+</ul>
