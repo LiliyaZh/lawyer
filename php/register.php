d9f7fd01d170eee649025ca40d7e4ee2cc4db3b6
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 11:50:51 +0300

    Добавлен шаблон формы регистрации клиента

---
+++ php/register.php
@@ -0,0 +1,27 @@
+<form method="post">
+    <div class="mb-3">
+        <label for="name" class="form-label">Имя</label>
+        <input type="text" class="form-control" id="name" name="name" required>
+    </div>
+    <div class="mb-3">
+        <label for="surname" class="form-label">Фамилия</label>
+        <input type="text" class="form-control" id="surname" name="surname" required>
+    </div>
+    <div class="mb-3">
+        <label for="tel" class="form-label">Телефон</label>
+        <input type="text" class="form-control" id="tel" name="tel" required>
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Пароль</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+    <div class="mb-3">
+        <label for="confirm_password" class="form-label">Подтвердите пароль</label>
+        <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Зарегистрироваться</button>
+</form>
