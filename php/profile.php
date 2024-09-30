6ccfa932708da353a1beb5cf24363d98c832b692
LiliyaZh <Lilya_sh@list.ru>
2024-09-30 13:22:13 +0300

    Добавлена бизнес-логика профиля

--- php/profile.php
+++ php/profile.php
@@ -1,3 +1,63 @@
+<?php
+require_once ('db.php');
+
+$del = false;
+if ($_SESSION['user_type'] == "client"){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['del'])) {
+        $key_of_client = $_SESSION['user_id'];
+
+        $sql = "DELETE FROM messages_in_requests WHERE key_of_client = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$key_of_client]);
+
+        $sql = "DELETE FROM files_of_requests WHERE key_of_client = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$key_of_client]);
+
+        $sql = "DELETE FROM feedbacks_of_clients WHERE key_of_client = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$key_of_client]);
+
+        $sql = "DELETE FROM requests_of_clients WHERE key_of_client = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$key_of_client]);
+
+        $sql = "DELETE FROM clients_of_company WHERE key_of_client = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$key_of_client]);
+
+        session_unset();
+        session_destroy();
+        header("Location: register?p=d");
+        exit();
+    }
+
+    $sql = "SELECT * FROM clients_of_company WHERE key_of_client = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_SESSION['user_id']]);
+    $client = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $type = "Клиент";
+    $name = $client['name_of_client'];
+    $surname = $client['surname_of_client'];
+    $telephone = $client['telephone_of_client'];
+    $email = $client['email_of_client'];
+    $del = true;
+}
+else if ($_SESSION['user_type'] == "employee"){
+    $sql = "SELECT * FROM workers_in_company WHERE key_of_worker = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_SESSION['user_id']]);
+    $worker = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $type = $_SESSION['empl_type'] == "1" ? "Администратор" : ($_SESSION['empl_type'] == "2" ? "Делопроизводитель" : ($_SESSION['empl_type'] == "3" ? "Юристконсульт" : "Ошибка"));
+    $name = $worker['name_of_worker'];
+    $surname = $worker['surname_of_worker'];
+    $telephone = $worker['telephone_of_worker'];
+    $email = $worker['email_of_worker'];
+}
+?>
+
 <h2 class="text-center mb-3">Данные профиля пользователя АИС</h2>
 <div class="row mb-3">
     <div class="col-3"></div>
